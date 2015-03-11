4ad9a0f5e6368a05ee8cba3e94a311e587bb96cf
nedal <nedal@bitbucket.org>
2015-03-11 19:35:34 +0200

    fix return url

--- class.onecard.php
+++ class.onecard.php
@@ -3,7 +3,7 @@
   Plugin Name: One Card
   Plugin URI: arabhosters.com
   Description: OneCard Payment extension for Woo-Commerece
-  Version: 1.2
+  Version: 1.3
   Author: Arabhosters
   Author URI: arabhosters.com
  */
@@ -184,7 +184,7 @@ function init_onecard_gateway_class() {
                 // Order key + ID
                 'OneCard_TransID' => $transaction_id,
                 'OneCard_Field1' => serialize(array($order_id, $order->order_key)),
-                'OneCard_ReturnURL' => add_query_arg('utm_nooverride', '1', $this->get_return_url($order)),
+                'OneCard_ReturnURL' => add_query_arg(array('pm_onecard_response'=>'pm_onecard','utm_nooverride'=>'1'), $this->get_return_url($order)),
                 'OneCard_Timein' => current_time('timestamp')
             );
 
